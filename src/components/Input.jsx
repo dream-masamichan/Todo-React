d2d1d3d9a90a9842781b31c1e0058db68e8b760e
dream-masamichan <positivemasa0624@gmail.com>
2025-03-30 13:06:11 +0900

    Styling Buttons & Inputs with Tailwind

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,10 +1,17 @@
 import React from 'react'
 
 function Input({ label, textarea, ...props }) {
+  const classes =
+    'w-full p-1 border-b-2 round-sm border-stone-300 bg-stone-200 text-stone-600 focus:outline focus:border-stone-600'
+
   return (
-    <p>
+    <p className="flex flex-col gap-1 my-4">
       <label>{label}</label>
-      {textarea ? <textarea {...props} /> : <input {...props} />}
+      {textarea ? (
+        <textarea className={classes} {...props} />
+      ) : (
+        <input className={classes} {...props} />
+      )}
     </p>
   )
 }
